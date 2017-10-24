5843b41bbb98c791e9cd67c99e12e712852ba601
EsromG5 <esromgalaxy5@gmail.com>
2017-10-24 18:22:33 -0200

    BringUP ViperOS

--- viper.mk
+++ viper.mk
@@ -21,7 +21,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := osprey
-PRODUCT_NAME := lineage_osprey
+PRODUCT_NAME := viper_osprey
 PRODUCT_MODEL := MotoG3
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
